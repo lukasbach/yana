d9b9ad18db1d2260d9a68d6ed4398fdc2c2ebb3e
Lukas Bach <lbach@outlook.de>
2020-10-29 02:51:23 +0100

    Collapsable sidebar

--- src/components/layout/LayoutContainer.tsx
+++ src/components/layout/LayoutContainer.tsx
@@ -10,8 +10,8 @@ import { MainContainer } from '../mainContent/MainContainer';
 import { TopBar } from './TopBar';
 // @ts-ignore
 import ResizePanel from "react-resize-panel";
-import { ResizeSensor } from '@blueprintjs/core';
-import { useState } from 'react';
+import { Button, Icon, ResizeSensor } from '@blueprintjs/core';
+import { useCallback, useRef, useState } from 'react';
 
 const styles = {
   mainContainer: cxs({
@@ -65,6 +65,16 @@ const styles = {
     height: '100%',
     width: '100%',
     overflow: 'auto',
+    position: 'relative',
+    paddingBottom: '32px',
+  }),
+  sidebarSizeButton: cxs({
+    height: '64px',
+    position: 'sticky',
+    bottom: '-32px',
+    left: 0,
+    textAlign: 'right',
+    padding: '16px'
   }),
   sidebarResizeBorder: cxs({
     width: '6px',
@@ -82,14 +92,34 @@ const styles = {
   }),
 };
 
+const SIZE_TO_COLLAPSE = 160;
+
 export const LayoutContainer: React.FC<{}> = props => {
   const theme = useTheme();
-  const [sidebarTooSmall, setSidebarTooSmall] = useState(false);
+  const lastSizeRef = useRef(0);
+  const [collapsed, setCollapsed] = useState(false);
+
+  const collapseButtonContainer = (
+    <div className={cx([
+      styles.sidebarSizeButton,
+      cxs({ background: `linear-gradient(0deg, ${theme.sidebarColor} 0%, ${Color(theme.sidebarColor).alpha(.95).toString()} 70%, rgba(0,0,0,0) 100%)` })
+    ])}>
+      <Button
+        icon={<Icon icon={collapsed ? 'chevron-right' : 'chevron-left'} iconSize={24} />}
+        onClick={() => setCollapsed(x => !x)}
+        minimal
+      />
+    </div>
+  );
 
   return (
     <div className={styles.mainContainer}>
       <TopBar />
-      <div className={cx(styles.centralContainer, 'app-central-container')}>
+      <div className={cx(
+        styles.centralContainer,
+        'app-central-container',
+        collapsed && lastSizeRef.current > SIZE_TO_COLLAPSE && cxs({ '> :first-child > :first-child': { width: '64px !important' } })
+      )}>
         <ResizePanel
           direction="e"
           handleClass={styles.sidebarResizeHandle}
@@ -112,13 +142,24 @@ export const LayoutContainer: React.FC<{}> = props => {
             )}
           >
             <ResizeSensor onResize={e => {
-              if (sidebarTooSmall && e[0].contentRect.width > 160) {
-                setSidebarTooSmall(false);
-              } else if (!sidebarTooSmall && e[0].contentRect.width <= 160) {
-                setSidebarTooSmall(true);
+              const width = e[0].contentRect.width
+              lastSizeRef.current = width;
+              if (collapsed && width > SIZE_TO_COLLAPSE) {
+                setCollapsed(false);
+              } else if (!collapsed && width <= SIZE_TO_COLLAPSE) {
+                setCollapsed(true);
               }
             }}>
-              { !sidebarTooSmall ? <SideBarContent /> : <div /> }
+              { !collapsed ? (
+                <div>
+                  <SideBarContent />
+                  { collapseButtonContainer }
+                </div>
+              ) : (
+                <div>
+                  { collapseButtonContainer }
+                </div>
+              ) }
             </ResizeSensor>
           </div>
         </ResizePanel>
--- src/components/sidebar/SideBarContent.tsx
+++ src/components/sidebar/SideBarContent.tsx
@@ -5,20 +5,16 @@ import { Button } from '@blueprintjs/core';
 import { DataItemKind } from '../../types';
 import { useEffect } from 'react';
 import { useDataSearch } from '../../datasource/useDataSearch';
-import { useDataInterface } from '../../datasource/DataInterfaceContext';
 import { InternalTag } from '../../datasource/InternalTag';
 import { SideBarTreeOnIds } from './SideBarTreeOnIds';
 import { NavigationTree } from './NavigationTree';
 
 export const SideBarContent: React.FC<{}> = props => {
-  const dataInterface = useDataInterface();
   const [rootCollection] = useDataSearch({ tags: [InternalTag.WorkspaceRoot] });
   const rootChilds = useDataSearch(rootCollection ? { parents: [rootCollection.id] } : {})
   console.log("rootchilds", rootCollection, rootChilds)
 
   useEffect(() => console.log(`Changed rootCollection`, rootCollection), [rootCollection])
-
-  const dummyItemContent = { childIds: [], kind: DataItemKind.NoteItem, tags: [], lastChange: 0, created: 0, };
 
   return (
     <>
@@ -39,16 +35,3 @@ export const SideBarContent: React.FC<{}> = props => {
     </>
   );
 };
-
-
-
-// <Button onClick={() => dataInterface.createDataItem({
-//   kind: DataItemKind.Collection,
-//   name: 'New Collection',
-//   tags: [],
-//   childIds: [],
-//   created: new Date().getTime(),
-//   lastChange: new Date().getTime()
-// })}>
-//   New Collection
-// </Button>
