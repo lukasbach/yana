3517a51bdf40ed5a2e0d8019e7c9be81cdfe5c8a
Lukas Bach <lbach@outlook.de>
2021-04-22 01:42:56 +0200

    Add issue link to about page

--- src/components/settings/about/AboutYanaPage.tsx
+++ src/components/settings/about/AboutYanaPage.tsx
@@ -3,6 +3,7 @@ import { SettingsSection } from '../layout/SettingsSection';
 import { SettingsClickable } from '../layout/SettingsClickable';
 import pkg from '../../../../package.json';
 import { remote } from 'electron';
+import cxs from 'cxs';
 
 export const AboutYanaPage: React.FC<{}> = props => {
   return (
@@ -15,6 +16,18 @@ export const AboutYanaPage: React.FC<{}> = props => {
           onClick={() => remote.shell.openExternal('https://github.com/lukasbach/yana')}
           icon={'globe'}
         />
+
+        <SettingsClickable
+          title="Report an issue"
+          subtitle="https://github.com/lukasbach/yana/issues"
+          onClick={() => remote.shell.openExternal('https://github.com/lukasbach/yana/issues')}
+          icon={'issue'}
+        />
+
+        <p className={cxs({ marginTop: '10px' })}>
+          We would be happy if you star Yana on Github if you enjoyed using it. Feel free to report
+          any problems, feedback or thoughts!
+        </p>
       </SettingsSection>
 
       <SettingsSection title="Maintainers">
